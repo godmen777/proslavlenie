9064ae61bdbfb7da1eba2a1e128cf6cfc589cc86
greenteamer <greenteamer@bk.ru>
2015-03-18 16:58:18 +0600

    Создание страниц со всеми новостями, отзывов, свидетельств

--- project/core/models.py
+++ project/core/models.py
@@ -10,7 +10,7 @@ from django.utils.translation import ugettext_lazy as _
 from ckeditor.fields import RichTextField
 from image_cropping import ImageRatioField
 
-
+"""Базовый класс"""
 class BaseArticle(models.Model):
     class Meta:
         abstract = True
@@ -19,6 +19,7 @@ class BaseArticle(models.Model):
     description = RichTextField()
 
 
+"""Блок слайдера"""
 # единица слайдера , связи 1 к 1 со статьями и со страницами
 class SliderItem(models.Model):
     name = models.CharField(max_length=200, verbose_name=u'Название слайдера')
@@ -35,6 +36,7 @@ class SliderItem(models.Model):
         return '/media/%s' % self.image
 
 
+"""Блок Статьи"""
 class Article(BaseArticle):
     sub_title = models.CharField(max_length=60, verbose_name=u'Подзаголовок')
     entry = models.TextField(verbose_name=u'Вступление')
@@ -53,7 +55,15 @@ class Article(BaseArticle):
     def url(self):
         return '/articles/%s' % self.slug
 
+class ArticleGalleryImage(models.Model):
+    article = models.ForeignKey(Article)
+    image = models.ImageField(verbose_name=u'Изображение для галереи', upload_to='gallery_article', blank=True)
+    cropping = ImageRatioField('image', '340x340')
+    def url(self):
+        return "/media/%s" % self.image
 
+
+"""Блок новости"""
 class News(models.Model):
     name = models.CharField(max_length=200, verbose_name=u'Название новости')
     description = RichTextField()
@@ -72,35 +82,6 @@ class News(models.Model):
     def url(self):
         return '/news/%s' % self.id
 
-
-
-
-class Review(models.Model):
-    name = models.CharField(max_length=200, verbose_name=u'Имя')
-    description = models.TextField(verbose_name=u'Отзыв')
-    date = models.DateField(verbose_name=u'Дата', default=datetime.datetime.now, editable=True)
-    class Meta:
-        verbose_name = u'Отзыв'
-        verbose_name_plural = u'Отзывы'
-    def __unicode__(self):
-        return _(u'Отзыв: ') + self.name
-
-
-class Page(BaseArticle):
-    slider = models.OneToOneField(SliderItem)
-    def __unicode__(self):
-        return _(u'Страница: ') + self.name
-
-
-
-class ArticleGalleryImage(models.Model):
-    article = models.ForeignKey(Article)
-    image = models.ImageField(verbose_name=u'Изображение для галереи', upload_to='gallery_article', blank=True)
-    cropping = ImageRatioField('image', '340x340')
-    def url(self):
-        return "/media/%s" % self.image
-
-
 class NewsGalleryImage(models.Model):
     news = models.ForeignKey(News)
     image = models.ImageField(verbose_name=u'Изображение для галереи', upload_to='gallery_news', blank=True)
@@ -109,6 +90,51 @@ class NewsGalleryImage(models.Model):
         return "/media/%s" % self.image
 
 
+"""Блок Отзывы"""
+class Review(models.Model):
+    name = models.CharField(max_length=200, verbose_name=u'Имя')
+    description = models.TextField(verbose_name=u'Отзыв')
+    input_video = models.CharField(max_length=240, verbose_name=u'поле для вставки видео из youtube')
+    date = models.DateField(verbose_name=u'Дата', default=datetime.datetime.now, editable=True)
+    image = models.ImageField(verbose_name=u'Изображение', upload_to='reviews', blank=True)
+    cropping = ImageRatioField('image', '275x200', verbose_name=u'Иконка')
+    class Meta:
+        verbose_name = u'Отзыв'
+        verbose_name_plural = u'Отзывы'
+    def __unicode__(self):
+        return _(u'Отзыв: ') + self.name
+    def url(self):
+        return u"/review/%s" % self.id
+
+
+"""Блок Свидетельства"""
+class Testimony(models.Model):
+    name = models.CharField(max_length=200, verbose_name=u'Ваше имя')
+    description = models.TextField(verbose_name=u'Ваше свидетельство')
+    image = models.ImageField(verbose_name=u'Фото для свидетельства', upload_to='gallery_testimony', blank=True)
+    class Meta:
+        verbose_name = u'Свидетельство'
+        verbose_name_plural = u'Свидетельства'
+    def __unicode__(self):
+        return u'Свидетельство %s' % self.name
+    def url(self):
+        return u'/testimony/%s' % self.id
+    def get_gallery_images(self):
+        return  TestimonyGalleryImage.objects.filter(testimony=self.id)
+
+class TestimonyGalleryImage(models.Model):
+    testimony = models.ForeignKey(Testimony)
+    image = models.ImageField(verbose_name=u'Изображение для галереи', upload_to='gallery_testimony', blank=True)
+    cropping = ImageRatioField('image', '175x120')
+    def url(self):
+        return "/media/%s" % self.image
+
+
+
+class Page(BaseArticle):
+    slider = models.OneToOneField(SliderItem)
+    def __unicode__(self):
+        return _(u'Страница: ') + self.name
 
 class PageGalleryImage(models.Model):
     page = models.ForeignKey(Page)
--- project/core/views.py
+++ project/core/views.py
@@ -11,7 +11,7 @@ from project.accounts.models import getOrganizerProfile
 from project.accounts.forms import OrganizerProfileForm, UserRegistrationForm
 from django.core.exceptions import ObjectDoesNotExist
 from django.http.response import Http404
-from project.core.models import Article, ArticleGalleryImage, Page, PageGalleryImage, News, SliderItem, Review
+from project.core.models import Article, ArticleGalleryImage, Page, PageGalleryImage, News, SliderItem, Review, Testimony
 from project.menu.models import MenuCategory
 
 
@@ -48,8 +48,29 @@ def newsView(request, id, template_name="catalog/news.html"):
     return render_to_response(template_name, locals(),
                               context_instance=RequestContext(request))
 
+def newsAllView(request, template_name="catalog/news_all.html"):
+    news_all = News.objects.all()
+    return render_to_response(template_name, locals(),
+                              context_instance=RequestContext(request))
 
 def reviewsView(request, template_name="catalog/reviews.html"):
     reviews = Review.objects.all()
+    return render_to_response(template_name, locals(),
+                              context_instance=RequestContext(request))
+
+def reviewView(request, id, template_name="catalog/review.html"):
+    review = Review.objects.get(id=id)
+    return render_to_response(template_name, locals(),
+                              context_instance=RequestContext(request))
+
+def testimonysView(request, template_name="catalog/reviews.html"):
+    testimonys = Testimony.objects.all()
+    return render_to_response(template_name, locals(),
+                              context_instance=RequestContext(request))
+
+def testimonyView(request, id, template_name="catalog/testimony.html"):
+    user = request.user
+    testimony = Testimony.objects.get(id=id)
+
     return render_to_response(template_name, locals(),
                               context_instance=RequestContext(request))
